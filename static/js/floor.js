65a91a67f6b17d638a8fddceffcb138bb9eb9200
jinzewang2014 <jinzewang2014@outlook.com>
2021-08-27 15:31:40 +0800

    完成js与html数据及样式修改

--- static/js/floor.js
+++ static/js/floor.js
@@ -1,87 +1,85 @@
-(function (func) {
-    $.ajax({
-        url: "/data/getFloor",
-        type: "GET",
-        dataType: "json",
-        success: function (data) {
-            func(data);
-        }
-    });
-}) (function (data) {
-    var dom = document.getElementById("chart_13");
-    var myChart = echarts.init(dom);
-    var app = {};
-    alert(data)
-    var option;
-    var dataFloor = [[43.3, 85.8, 93.7]
-                    ,[83.1, 73.4, 55.1]
-                    ,[86.4, 65.2, 82.5]
-                    ,[72.4, 53.9, 39.1]
-                    ,[23.1, 45.2, 15.2]
-                    ,[14,45,36]
-                    ,[14,45,36]
-                    ,[14,45,36]
-                    ,[14,45,36]]
-
-    option = {
-        legend: {},
-        tooltip: {},
-        dataset: {
-            source: [
-                ['product', '高楼层', '中楼层', '低楼层'],
-                ['渝中', dataFloor[0][0], dataFloor[0][1], dataFloor[0][2]],
-                ['江北', dataFloor[1][0], dataFloor[1][1], dataFloor[1][2]],
-                ['渝北', dataFloor[2][0], dataFloor[2][1], dataFloor[2][2]],
-                ['南岸', dataFloor[3][0], dataFloor[3][1], dataFloor[3][2]],
-                ['沙坪坝', dataFloor[4][0], dataFloor[4][1], dataFloor[4][2]],
-                ['九龙坡',dataFloor[5][0], dataFloor[5][1], dataFloor[5][2]],
-                ['大渡口',dataFloor[6][0], dataFloor[6][1], dataFloor[6][2]],
-                ['北碚', dataFloor[7][0], dataFloor[7][1], dataFloor[7][2]],
-                ['巴南', dataFloor[8][0], dataFloor[8][1], dataFloor[8][2]],
+$.getJSON('/data/getFloor', function (view_data) {
+    (function () {
+        var dom = document.getElementById("chart_13");
+        var myChart = echarts.init(dom);
+        var app = {};
+        var option;
+        option = {
+            legend: {
+                textStyle: {
+                    color: '#fdf0f0',
+                    font: 14,
+                }
+            },
+            tooltip: {},
+            dataset: {
+                source: [
+                    ['product', '高楼层', '中楼层', '低楼层'],
+                    ['渝中', view_data['view_data'][0][0], view_data['view_data'][0][1], view_data['view_data'][0][2]],
+                    ['江北', view_data['view_data'][1][0], view_data['view_data'][1][1], view_data['view_data'][1][2]],
+                    ['渝北', view_data['view_data'][2][0], view_data['view_data'][2][1], view_data['view_data'][2][2]],
+                    ['南岸', view_data['view_data'][3][0], view_data['view_data'][3][1], view_data['view_data'][3][2]],
+                    ['沙坪坝', view_data['view_data'][4][0], view_data['view_data'][4][1], view_data['view_data'][4][2]],
+                    ['九龙坡',view_data['view_data'][5][0], view_data['view_data'][5][1], view_data['view_data'][5][2]],
+                    ['大渡口',view_data['view_data'][6][0], view_data['view_data'][6][1], view_data['view_data'][6][2]],
+                    ['北碚', view_data['view_data'][7][0], view_data['view_data'][7][1], view_data['view_data'][7][2]],
+                    ['巴南', view_data['view_data'][8][0], view_data['view_data'][8][1], view_data['view_data'][8][2]],
+                ]
+            },
+            xAxis: {
+                axisLabel: {
+                    textStyle: {
+                        color: '#ffffff'
+                    },
+                },
+                type: 'category'
+            },
+            yAxis: {
+                axisLabel: {
+                    textStyle: {
+                        color: '#ffffff'
+                    },
+                },
+            },
+            // Declare several bar series, each will be mapped
+            // to a column of dataset.source by default.
+            series: [
+                {type: 'bar'},
+                {type: 'bar'},
+                {type: 'bar'},
             ]
-        },
-        xAxis: {
-            type: 'category'
-        },
-        yAxis: {},
-        // Declare several bar series, each will be mapped
-        // to a column of dataset.source by default.
-        series: [
-            {type: 'bar'},
-            {type: 'bar'},
-            {type: 'bar'},
-        ]
-    };
+        };
 
-    if (option && typeof option === 'object') {
-        myChart.setOption(option);
-    }
-    // var chartDom = document.getElementById('chart_13');
-    // var myChart = echarts.init(chartDom);
-    // var option;
-    //
-    // option = {
-    //     legend: {},
-    //     tooltip: {},
-    //     dataset: {
-    //         source: [
-    //             ['product', '2015', '2016', '2017'],
-    //             ['Matcha Latte', 43.3, 85.8, 93.7],
-    //             ['Milk Tea', 83.1, 73.4, 55.1],
-    //             ['Cheese Cocoa', 86.4, 65.2, 82.5],
-    //             ['Walnut Brownie', 72.4, 53.9, 39.1]
-    //         ]
-    //     },
-    //     xAxis: {type: 'category'},
-    //     yAxis: {},
-    //     // Declare several bar series, each will be mapped
-    //     // to a column of dataset.source by default.
-    //     series: [
-    //         {type: 'bar'},
-    //         {type: 'bar'},
-    //         {type: 'bar'}
-    //     ]
-    // };
-    //
-    // option && myChart.setOption(option);
+        if (option && typeof option === 'object') {
+            myChart.setOption(option);
+        }
+        // var chartDom = document.getElementById('chart_13');
+        // var myChart = echarts.init(chartDom);
+        // var option;
+        //
+        // option = {
+        //     legend: {},
+        //     tooltip: {},
+        //     dataset: {
+        //         source: [
+        //             ['product', '2015', '2016', '2017'],
+        //             ['Matcha Latte', 43.3, 85.8, 93.7],
+        //             ['Milk Tea', 83.1, 73.4, 55.1],
+        //             ['Cheese Cocoa', 86.4, 65.2, 82.5],
+        //             ['Walnut Brownie', 72.4, 53.9, 39.1]
+        //         ]
+        //     },
+        //     xAxis: {type: 'category'},
+        //     yAxis: {},
+        //     // Declare several bar series, each will be mapped
+        //     // to a column of dataset.source by default.
+        //     series: [
+        //         {type: 'bar'},
+        //         {type: 'bar'},
+        //         {type: 'bar'}
+        //     ]
+        // };
+        //
+        // option && myChart.setOption(option);
+    })();
 })
--- static/js/house_size.js
+++ static/js/house_size.js
@@ -1,106 +1,108 @@
-(function (func) {
-    $.ajax({
-        url: "/data/getHouseSize",
-        type: "GET",
-        dataType: "json",
-        success: function (data) {
-            func(data);
-        }
-    });
-}) (function (data) {
-    var chartDom = document.getElementById('chart_16');
-    var myChart = echarts.init(chartDom);
-    var option;
-    var dateArea = [[320, 302, 301, 334, 390, 330, 320, 300, 250]
-                    ,[120, 132, 101, 134, 90, 230, 210, 300, 250]
-                    ,[220, 182, 191, 234, 290, 330, 310, 300, 250]
-                    ,[150, 212, 201, 154, 190, 330, 410, 300, 250]]
-    option = {
-        tooltip: {
-            trigger: 'axis',
-            axisPointer: {            // Use axis to trigger tooltip
-                type: 'shadow'        // 'shadow' as default; can also be 'line' or 'shadow'
-            }
-        },
-        legend: {
-            data: ['50以下', '50-100', '100-200', '200以上']
-        },
-        grid: {
-            left: '3%',
-            right: '4%',
-            bottom: '3%',
-            containLabel: true
-        },
-        xAxis: {
-            type: 'value'
-        },
-        yAxis: {
-            type: 'category',
-            data: ['渝中', '江北', '渝北', '南岸', '沙坪坝', '九龙坡', '大渡口','北碚', '巴南']
-        },
-        series: [
-            {
-                name: '50以下',
-                type: 'bar',
-                stack: 'total',
-                label: {
-                    show: true
-                },
-                emphasis: {
-                    focus: 'series'
-                },
-                data: dateArea[0]//形成各区域的列表数据data.service,可以是列表中的列表
+$.getJSON('/data/getHouseSize', function (view_data) {
+    (function () {
+        var chartDom = document.getElementById('chart_16');
+        var myChart = echarts.init(chartDom);
+        var option;
+        option = {
+            tooltip: {
+                trigger: 'axis',
+                axisPointer: {            // Use axis to trigger tooltip
+                    type: 'shadow'        // 'shadow' as default; can also be 'line' or 'shadow'
+                }
             },
-            {
-                name: '50-100',
-                type: 'bar',
-                stack: 'total',
-                label: {
-                    show: true
+            legend: {
+                textStyle: {
+                    color: '#f5e9e9',
                 },
-                emphasis: {
-                    focus: 'series'
-                },
-                data: dateArea[1]
+                data: ['50以下', '50-100', '100-200', '200以上']
             },
-            {
-                name: '100-200',
-                type: 'bar',
-                stack: 'total',
-                label: {
-                    show: true
-                },
-                emphasis: {
-                    focus: 'series'
-                },
-                data: dateArea[2]
+            grid: {
+                left: '3%',
+                right: '4%',
+                bottom: '3%',
+                containLabel: true
             },
-            {
-                name: '200以上',
-                type: 'bar',
-                stack: 'total',
-                label: {
-                    show: true
+            xAxis: {
+                axisLabel: {
+                    textStyle: {
+                        color: '#ffffff'
+                    },
                 },
-                emphasis: {
-                    focus: 'series'
-                },
-                data: dateArea[3]
+                type: 'value'
             },
-            // {
-            //     name: 'Search Engine',
-            //     type: 'bar',
-            //     stack: 'total',
-            //     label: {
-            //         show: true
-            //     },
-            //     emphasis: {
-            //         focus: 'series'
-            //     },
-            //     data: [820, 832, 901, 934, 1290, 1330, 1320]
-            // }
-        ]
-    };
-
-    option && myChart.setOption(option);
+            yAxis: {
+                axisLabel: {
+                    textStyle: {
+                        color: '#ffffff'
+                    },
+                },
+                type: 'category',
+                data: ['渝中', '江北', '渝北', '南岸', '沙坪坝', '九龙坡', '大渡口','北碚', '巴南']
+            },
+            series: [
+                {
+                    name: '50以下',
+                    type: 'bar',
+                    stack: 'total',
+                    label: {
+                        show: true
+                    },
+                    emphasis: {
+                        focus: 'series'
+                    },
+                    data: view_data[0]//形成各区域的列表数据data.service,可以是列表中的列表
+                },
+                {
+                    name: '50-100',
+                    type: 'bar',
+                    stack: 'total',
+                    label: {
+                        show: true
+                    },
+                    emphasis: {
+                        focus: 'series'
+                    },
+                    data: view_data[1]
+                },
+                {
+                    name: '100-200',
+                    type: 'bar',
+                    stack: 'total',
+                    label: {
+                        show: true
+                    },
+                    emphasis: {
+                        focus: 'series'
+                    },
+                    data: view_data[2]
+                },
+                {
+                    name: '200以上',
+                    type: 'bar',
+                    stack: 'total',
+                    label: {
+                        show: true
+                    },
+                    emphasis: {
+                        focus: 'series'
+                    },
+                    data: view_data[3]
+                },
+                // {
+                //     name: 'Search Engine',
+                //     type: 'bar',
+                //     stack: 'total',
+                //     label: {
+                //         show: true
+                //     },
+                //     emphasis: {
+                //         focus: 'series'
+                //     },
+                //     data: [820, 832, 901, 934, 1290, 1330, 1320]
+                // }
+            ]
+        };
+    
+        option && myChart.setOption(option);
+    })()
 })

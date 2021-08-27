537445cd8d8ab89855e051c4ec9b0e45e47bd92c
阿萨德 <1044511037@qq.com>
2021-08-27 15:50:19 +0800

    修改

--- view/data_view.py
+++ view/data_view.py
@@ -11,12 +11,17 @@ from dbmodel.floor_location import FloorLocation
 from dbmodel.house_size import HouseSize
 
 data = Blueprint('data', __name__)
-
+conn = pymysql.connect(host="10.20.220.203", user="root", password="sx2626", database="python",
+                       charset='utf8')
+cursor1 = conn.cursor()
+cursor1.execute('select houses_avgUnitPrice,houses_area from avgprice')
+cursor2 = conn.cursor()
+cursor2.execute('select houses_HouseType,number from housestype')
 
 @data.route("/getFloor", methods=['GET'])
 def get_floor():
     conn = pymysql.connect(host="localhost", user="root", password="root", database="cqhouseanalyze",
-                                   charset='utf8')
+                           charset='utf8')
     cursor = conn.cursor()
     cursor.execute('select floor_top, floor_mid, floor_low from floor_location')
     data = cursor.fetchall()
@@ -27,7 +32,7 @@ def get_floor():
 @data.route("/getHouseSize", methods=['GET'])
 def get_house_size():
     conn = pymysql.connect(host="localhost", user="root", password="root", database="cqhouseanalyze",
-                                   charset='utf8')
+                           charset='utf8')
     cursor = conn.cursor()
     cursor.execute('select interval0, interval1, interval2, interval3 from house_size')
     data = cursor.fetchall()
@@ -48,44 +53,33 @@ def get_house_size():
     return json.dumps(data, ensure_ascii=False)
 
 
-# @data.route('/getAvg', methods=['GET'])
-# def get_avg():
-#     conn = pymysql.connect(host="10.20.220.203", user="root", password="sx2626", database="python",
-#                                    charset='utf8')
-#     cursor = conn.cursor()
-#     cursor.execute('select houses_avgUnitPrice from avgprice')
-#     price = cursor.fetchall()
-#     for i in range(len(price)):
-#         price[i] = price[i][0]
-#     cursor.execute('select houses_area from avgprice')
-#     area = cursor.fetchall()
-#     for i in range(len(area)):
-#         area[i] = area[i][0]
-#     avg = {
-#         'price': price,
-#         'area': area
-#     }
-#     avg = json.dumps(avg)
-#     return avg
-#
-#
-# @data.route('/getType', methods=['GET'])
-# def get_type():
-#     conn = pymysql.connect(host="10.20.220.203", user="root", password="sx2626", database="python",
-#                                    charset='utf8')
-#     cursor = conn.cursor()
-#     cursor.execute('select number from housestype')
-#     num = cursor.fetchall()
-#     # price=price(lambda x:x[0])
-#     for i in range(len(num)):
-#         num[i] = num[i][0]
-#     cursor.execute('select houses_HouseType from housestype')
-#     type = cursor.fetchall()
-#     for i in range(len(type)):
-#         type[i] = type[i][0]
-#     data = {
-#         'num': num,
-#         'type': type
-#     }
-#     data = json.dumps(data)
-#     return data
+@data.route('/getAvg', methods=['GET'])
+def get_avg():
+    price = cursor1.fetchall()
+    avg = {}
+    area = {}
+    for i in range(len(price)):
+        avg[i] = price[i][0]
+        area[i] = price[i][1]
+    avgs = {
+        'avg': avg,
+        'area': area
+    }
+    avgs = json.dumps(avgs)
+    return avgs
+
+
+@data.route('/getType', methods=['GET'])
+def get_type():
+    num = cursor2.fetchall()
+    numb = {}
+    type = {}
+    for i in range(len(num)):
+        type[i] = num[i][0]
+        numb[i] = num[i][1]
+    data = {
+        'numb': numb,
+        'type': type
+    }
+    data = json.dumps(data)
+    return data

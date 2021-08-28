f9c347e3f4990aeed716680c16513f59549d44a0
ky <1348754914@qq.com>
2021-08-28 09:50:10 +0800

    关注度与装修情况单间关系

--- static/js/gz.js
+++ static/js/gz.js
@@ -1,33 +1,148 @@
-// 柱状图1模块
+// 关注度数据展示
+
 
 // 关注度数据展示
+$.getJSON('/data/get_relation_data', function (relation) {
+    (function () {
+
+        // 基于准备好的dom，初始化echarts实例
+        var myChart = echarts.init(document.getElementById("zx"));
+        var data1 = relation.slice(0, 9)
+        var data2 = relation.slice(9, 18)
+        var data3 = relation.slice(18, 27)
+        var data4 = relation.slice(27, 36)
+        // console.log(data1,data2,data3,data4)
+        option = {
+            title: {},
+            tooltip: {
+                trigger: 'axis',
+                axisPointer: {
+                    type: 'shadow'
+                }
+            },
+            legend: {
+                data: ['精装', '简装', '毛坯'],
+                textStyle: {
+                    color: '#ffffff',
+                    fontSize: 12
+                }
+            },
+            grid: {
+                x: 45,
+                y: 20,
+                x2: 1,
+                y2: 18,
+            },
+            xAxis: {
+                type: 'value',
+                boundaryGap: [0, 0.01],
+                axisLabel: {color: '#fff'},
+                axisLabel: {
+                    textStyle: {
+                        color: '#c3dbff',  //更改坐标轴文字颜色
+                        fontSize: 12      //更改坐标轴文字大小
+                    }
+                }
+
+            },
+            yAxis: {
+                type: 'category',
+                data: data1,
+                axisLine: {show: false},
+                axisTick: {show: false},
+                axisLabel: {color: '#fff'}
+            },
+            series: [
+                {
+                    name: '精装',
+                    type: 'bar',
+                    data: data3,
+                    itemStyle: {
+                        barBorderRadius: 3
+                    },
+
+                },
+                {
+                    name: '简装',
+                    type: 'bar',
+                    data: data4,
+                    itemStyle: {
+                        barBorderRadius: 3
+                    }
+                },
+                {
+                    name: '毛坯',
+                    type: 'bar',
+                    data: data2,
+                    itemStyle: {
+                        barBorderRadius: 3
+                    }
+                },
+
+            ]
+        };
+
+        myChart.setOption(option);
+    })();
+});
+
 $.getJSON('/data/getGZ', function (gz) {
     (function () {
         // 基于准备好的dom，初始化echarts实例
-        var myChart = echarts.init(document.querySelector("bar.chart"));
-        var sumPeople = gz['SumPeople']
-        var C_area = gz['C_area']
+        var myChart = echarts.init(document.getElementById("gz"));
+        var sumPeople = gz.slice(9, 18)
+        var C_area = gz.slice(0, 9)
 
+        // console.log(sumPeople, C_area)
         var data1 = sumPeople
         var data2 = ['关注人数']
         var data3 = data2.concat(data1)
         var data4 = ['product']
         var data5 = C_area
         var data6 = data4.concat(data5)
+        console.log(data3, data6)
         option = {
-            legend: {},
+            legend: {
+                textStyle: {
+                    color: '#ffffff'
+                }
+            },
             tooltip: {},
             dataset: {
                 source: [
                     data6, data3,
                 ]
             },
-            xAxis: {type: 'category'},
-            yAxis: {},
+            grid: {
+                x: 55,
+                y: 52,
+                x2: 1,
+                y2: 18,
+            },
+            xAxis: {
+                type: 'category',
+                axisLabel: {
+                    textStyle: {
+                        color: '#c3dbff',  //更改坐标轴文字颜色
+                        fontSize: 12      //更改坐标轴文字大小
+                    }
+                }
+            },
+            yAxis: {
+                axisLabel: {
+                    textStyle: {
+                        color: '#c3dbff',  //更改坐标轴文字颜色
+                        fontSize: 12      //更改坐标轴文字大小
+                    }
+                }
+            },
             // Declare several bar series, each will be mapped
             // to a column of dataset.source by default.
             series: [
-                {type: 'bar'},
+                {
+                    type: 'bar',
+                    barGap: '0.5'
+                },
                 {type: 'bar'},
                 {type: 'bar'},
                 {type: 'bar'},
@@ -40,76 +155,8 @@ $.getJSON('/data/getGZ', function (gz) {
             ]
         };
 
-
         // 3. 把配置和数据给实例对象
         myChart.setOption(option);
 
-        // 重新把配置好的新数据给实例对象
-        myChart.setOption(option);
-        window.addEventListener("resize", function () {
-            myChart.resize();
-        });
     })();
-    console.log(data['price'][0])
 });
-
-//
-// // 关注度数据展示
-// $.getJSON('/data/get_relation_data', function (gz) {
-//     (function () {
-//         // 基于准备好的dom，初始化echarts实例
-//         var myChart = echarts.init(document.querySelector("bar.chart"));
-//
-//         var data1 = {
-//         {
-//             GZ | tojson
-//         }
-//     }
-//         var data2 = ['关注人数']
-//         var data3 = data2.concat(data1)
-//         var data4 = ['product']
-//         var data5 = {
-//         {
-//             area | tojson
-//         }
-//     }
-//         var data6 = data4.concat(data5)
-//         option = {
-//             legend: {},
-//             tooltip: {},
-//             dataset: {
-//                 source: [
-//                     data6, data3,
-//                 ]
-//             },
-//             xAxis: {type: 'category'},
-//             yAxis: {},
-//             // Declare several bar series, each will be mapped
-//             // to a column of dataset.source by default.
-//             series: [
-//                 {type: 'bar'},
-//                 {type: 'bar'},
-//                 {type: 'bar'},
-//                 {type: 'bar'},
-//                 {type: 'bar'},
-//                 {type: 'bar'},
-//                 {type: 'bar'},
-//                 {type: 'bar'},
-//                 {type: 'bar'},
-//
-//             ]
-//         };
-//
-//
-//         // 3. 把配置和数据给实例对象
-//         myChart.setOption(option);
-//
-//         // 重新把配置好的新数据给实例对象
-//         myChart.setOption(option);
-//         window.addEventListener("resize", function () {
-//             myChart.resize();
-//         });
-//     })();
-//     console.log(data['price'][0])
-// });
-//
--- view/data_view.py
+++ view/data_view.py
@@ -1,5 +1,7 @@
 # #coding=gbk
 import json
+
+import numpy as np
 import pymysql
 from config import db
 
@@ -7,8 +9,7 @@ from config import db
 本视图专门用于处理ajax数据
 """
 from flask import Blueprint
-from dbmodel.floor_location import FloorLocation
-from dbmodel.house_size import HouseSize
+
 
 data = Blueprint('data', __name__)
 conn = pymysql.connect(host="10.20.220.203", user="root", password="sx2626", database="python",
@@ -17,6 +18,11 @@ cursor1 = conn.cursor()
 cursor1.execute('select houses_avgUnitPrice,houses_area from avgprice')
 cursor2 = conn.cursor()
 cursor2.execute('select houses_HouseType,number from housestype')
+
+cursot_ky1 = conn.cursor()
+cursot_ky1.execute("select * from gz;")
+cursot_ky2 = conn.cursor()
+cursot_ky2.execute("select area,avg from zx;")
 
 @data.route("/getFloor", methods=['GET'])
 def get_floor():
@@ -83,3 +89,52 @@ def get_type():
     }
     data = json.dumps(data)
     return data
+
+
+@data.route('/getGZ', methods=['GET'])
+def get_gz():
+
+    row1 = cursot_ky1.fetchall()
+    row2 = np.array(row1)
+    row3 = row2.tolist()    #转换为列表
+    C_area=[]
+    SumPeople=[]
+
+    for i in row3:
+        C_area.append(i[0])
+        SumPeople.append(i[1])
+
+    gz = C_area+SumPeople
+    print(gz)
+    gz=json.dumps(gz)
+
+    return gz
+
+@data.route('/get_relation_data', methods=['GET'])
+
+def get_relation():
+#重庆各区二手房装修情况与单价间关系
+    conn = pymysql.connect(host='10.20.220.203', user='root', passwd='sx2626', port=3306, db='python', charset='utf8')
+
+    data_2 = cursot_ky2.fetchall()
+    # print(data_2)
+
+    data_3 = np.array(data_2)
+    data_4= data_3.tolist()    #转换为列表
+    print(data_4)
+    data=[]
+    area=[]
+
+    for i in data_4:
+        data.append(i[1])
+        area.append(i[0])
+
+    print(area)
+    area_data=area[0::4]
+    mp_data=data[1::4]
+    jianz_data=data[2::4]
+    jinz_data=data[3::4]
+    relation_data=area_data+mp_data+jianz_data+jinz_data
+    print(relation_data)
+    relation=json.dumps(relation_data)
+    return relation
